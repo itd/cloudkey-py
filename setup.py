94969f58719df0ad0690f63d67f0424aed7a3e11
Olivier Poitrey <rs@dailymotion.com>
2010-04-15 14:43:19 +0200

    Add dependancies to setup.py and use setuptools instead of distutuils

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='cloudkey', version='1.0', py_modules=['cloudkey'])
+setup(name='cloudkey',
+      description='Dailymotion Cloud API client library',
+      author='Dailymotion',
+      url='http://github.com/dailymotion/cloudkey-py',
+      version='1.0',
+      install_requires=['simplejson>=2.1.1', 'pycurl>=7.19.0'],
+      zip_safe=True,
+      test_suite='tests')
 
