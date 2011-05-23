bbe607d7e603c3bc1697d69dab104c285da86cf5
Kurt Bendl <kurt@tool.net>
2011-05-23 10:06:19 -0600

    Removed old nosetest.sh file and setup a 'test' dir for later.

--- cloudkey/nosetest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-nosetests --with-coverage --cover-package=cloudkey --cover-html --cover-html-dir=cover tests.py
-cd cover
-python -m SimpleHTTPServer 8888
---
+++ cloudkey/test/README.txt
@@ -0,0 +1,1 @@
+# Tests can go here.
